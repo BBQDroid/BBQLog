720cc3fa1e7ac72aad1e714fb59b015df8587df0
xplodwild <xplod@ouverta.fr>
2012-05-22 10:25:14 +0300

    Cleanup and optimization

--- changesets.php
+++ changesets.php
@@ -1,11 +1,13 @@
 <?php
 
+// Initial config
 error_reporting(E_ALL & ~E_NOTICE);
 
 require_once("config.php");
 mysql_connect($CFG['SQL']['Host'], $CFG['SQL']['User'], $CFG['SQL']['Pass']);
 mysql_select_db($CFG['SQL']['DB']);
 
+// Prepare the CommitDate WHERE clause
 if (!empty($_GET['sortCode']))
 	$commitDateClause = " commits.CommitDate < '" . date("Y-m-d H:i:s", $_GET['sortCode']) . "' AND ";
 else if (!empty($_GET['startDate']) && !empty($_GET['endDate']))
@@ -15,8 +17,13 @@ else if (!empty($_GET['endDate']))
 else
 	$commitDateClause = "";
 
+// Fetch the Rom/Version ID couple from the DB based on the values passed
 $RomName = mysql_real_escape_string($_GET['RomName']);
 $RomVersion = mysql_real_escape_string($_GET['Version']);
+$Amount = intval($_GET['amount']);
+
+if ($Amount <= 0 || $Amount > 50) 
+	$Amount = 50;
 
 $commits = mysql_query("
     SELECT * FROM (SELECT * FROM repositories WHERE IDRomVersion=
@@ -32,19 +39,25 @@ $commits = mysql_query("
     commits.Repository=repos.Repository AND
     commits.Branch=repos.Branch
     ORDER BY CommitDate DESC
-    LIMIT 30
+    LIMIT $Amount
 ");
 
 $jsonData = array();
 
+// Output commits with a structure similar to Gerrit's one
 while ($commit = mysql_fetch_assoc($commits)) {
 	$message = $commit['Message'];
-	if (strpos($message, "\n") != 0)
-		$message = substr($message, 0, strpos($message, "\n"));
+	
+	// cut the commit message at the first newline
+	$newLinePos = strpos($message, "\n");
+	if ($newLinePos != 0)
+		$message = substr($message, 0, $newLinePos);
 
 	$jsonData["result"]["changes"][] = array("id"=>0, "project"=>array("key"=>array("name"=>$commit['Repository'])), "lastUpdatedOn"=>$commit['CommitDate'], "subject"=>$message, "sortKey"=>strtotime($commit['CommitDate']), "repository"=>$commit['Repository'], "sha"=>$commit['SHA'], "gituser"=>$commit['GitUsername']);
 }
 
+
 echo json_encode($jsonData);
+mysql_close();
 
 ?>
