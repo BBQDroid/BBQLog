8b91c72b45deac66d66b90ca3661de884898599e
nebkat <nebkat@teamhacksung.org>
2012-05-23 15:43:05 +0200

    Add possibility to disable troll

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,5 +1,5 @@
 (function($) {
-	$.fn.konami = function(callback, code) {
+	$.fn.konami = function(callback, code, stay) {
 		if(code == undefined) code = "38,38,40,40,37,39,37,39,66,65";
 		
 		return this.each(function() {
@@ -10,7 +10,9 @@
 					kkeys.shift();
 				}
 				if ( kkeys.toString().indexOf( code ) >= 0 ){
-					$(this).unbind('keydown', arguments.callee);
+					if (!stay) {
+						$(this).unbind('keydown', arguments.callee);
+					}
 					callback(e);
 				}
 			});
@@ -22,17 +24,24 @@
 $(document).ready(function(){
 	$(window).konami(function(){
 		alert("netchip sux");
-	}, "78,69,84,67,72,73,80");
-	$(window).konami(function(){
-		setInterval(function(){
+	}, "78,69,84,67,72,73,80", true);
+	var nebkatTroll = function(){
+		var smallTrollInterval = setInterval(function(){
 			var x = Math.floor((Math.random()*($(window).width() + 100))+1) - 50;
 			var y = Math.floor((Math.random()*($(window).height() + 100))+1) - 50;
-			$("<img src='images/troll.png' style='position:fixed; left:"+x+"px; top:"+y+"px;'></img>").appendTo($("body"));
+			$("<img src='images/troll.png' class='troll_image' style='position:fixed; left:"+x+"px; top:"+y+"px;'></img>").appendTo($("body"));
 		}, 1);
-		setTimeout(function(){
+		var largeTrollTimeout = setTimeout(function(){
 			var x = $(window).width() / 2 - 240;
 			var y = $(window).height() / 2 - 240;
-			$("<img src='images/troll_large.png' style='z-index:100000000; position:fixed; left:"+x+"px; top:"+y+"px;'></img>").appendTo($("body"));
+			$("<img src='images/troll_large.png' class='troll_image' style='z-index:100000000; position:fixed; left:"+x+"px; top:"+y+"px;'></img>").appendTo($("body"));
 		}, 5000);
-	}, "78,69,66,75,65,84");
+		$(window).konami(function(){
+			clearInterval(smallTrollInterval);
+			clearTimeout(largeTrollTimeout);
+			$(".troll_image").remove();
+			$(window).konami(nebkatTroll, "78,69,66,75,65,84");
+		}, "84,65,75,66,69,78");
+	};
+	$(window).konami(nebkatTroll, "78,69,66,75,65,84");
 });
